bca5a503a1009d3b03b12e6c55a426d9c7e61fe8
mr.one.id <101521147+onepunya@users.noreply.github.com>
2025-12-14 04:16:57 +0700

    Add files via upload

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,24 +4,28 @@ import {
   useSelector as useAppSelector,
 } from "react-redux";
 import { persistStore, persistReducer } from "redux-persist";
-import rootReducer, { rootPersistConfig } from "./root-reducer";
-import { baseAnimeApi, baseComicApi, baseMovieApi } from "./axios-base-query";
 
-const store = configureStore({
+import rootReducer, { rootPersistConfig } from "./root-reducer";
+import { baseAnimeApi } from "./axios-base-query";
+
+export const store = configureStore({
   reducer: persistReducer(rootPersistConfig, rootReducer),
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
       immutableCheck: false,
-    }).concat(
-      baseAnimeApi.middleware,
-      baseComicApi.middleware,
-      baseMovieApi.middleware
-    ),
+    }).concat(baseAnimeApi.middleware),
 });
 
-const persistor = persistStore(store);
-const { dispatch } = store;
-const useSelector = useAppSelector;
-const useDispatch = () => useAppDispatch();
-export { store, persistor, dispatch, useSelector, useDispatch };
+export const persistor = persistStore(store);
+
+/**
+ * Typed helpers
+ */
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useSelector = useAppSelector;
+export const useDispatch = () => useAppDispatch<AppDispatch>();
+
+export const { dispatch } = store;
